5a3fdce107bef1d5fb753b041224e9231666fc9b
pablo-blancoc <pablo_blancoc@outlook.es>
2020-11-20 19:24:01 -0600

    added test card for logs

--- app/src/app/pages/home/home.page.ts
+++ app/src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  registro = {
+    hora: "",
+    entrada: false,
+    ubicacion: "",
+  }
+
+  data: any[] = Array();
+
+  @ViewChild( IonInfiniteScroll ) infiniteScroll: IonInfiniteScroll;
+
+  constructor() {}
 
   ngOnInit() {
+  }
+
+  loadData( event ) {
+
+    setTimeout(() => {
+      console.log('Done');
+
+      for (let index = 0; index < 20; index++) {
+        let registro = {
+          hora: "hora de prueba",
+          entrada: index % 2 == 0,
+          ubicacion: "ubicación de prueba",
+        }
+        this.data.push(registro);
+      }
+
+      this.infiniteScroll.complete();
+
+      if (this.data.length == 100) {
+        this.infiniteScroll.disabled = true;
+        console.log("MAX ELEMENTS");
+      }
+    }, 500);
+
   }
 
 }
